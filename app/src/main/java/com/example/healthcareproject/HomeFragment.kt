77fe46047bc7d2c8ca82e09d0e40685ca2ad5097
Anh Nguyen Duc <116078764+ducahn2004@users.noreply.github.com>
2025-04-09 09:19:30 +0700

    Update HomeFragment.kt

--- app/src/main/java/com/example/healthcareproject/HomeFragment.kt
+++ app/src/main/java/com/example/healthcareproject/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.healthcareproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +22,7 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("StringFormatInvalid")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
