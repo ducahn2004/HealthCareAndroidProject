07bce9eb08d318b26de8191843d5dfb68cdcc2de
TuanAnhSatoh <tuananhdx9a@gmail.com>
2025-04-16 09:30:52 +0700

    Mapper

---
+++ app/src/main/java/com/example/healthcareproject/data/mapper/EmergencyInfoMaper.kt
@@ -0,0 +1,5 @@
+package com.example.healthcareproject.data.mapper
+
+import com.example.healthcareproject.data.source.local.entity.EmergencyInfo
+import com.example.healthcareproject.data.source.network.model.FirebaseEmergencyInfo
+
---
+++ app/src/main/java/com/example/healthcareproject/data/mapper/UserMapper.kt
@@ -0,0 +1,6 @@
+package com.example.healthcareproject.data.mapper
+
+import com.example.healthcareproject.data.source.local.entity.User
+import com.example.healthcareproject.data.source.network.model.FirebaseUser
+
+
