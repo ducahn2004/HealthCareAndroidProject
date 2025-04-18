543820bcc21a68fdbc7cf7ef4e6f9a466851aa87
Anh Nguyen Duc <116078764+ducahn2004@users.noreply.github.com>
2025-04-18 19:25:49 +0700

    Update remove all to present file

---
+++ app/src/main/java/com/example/healthcareproject/present/medicine/MedicationAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.healthcareproject.present.medicine
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.healthcareproject.databinding.ItemMedicationBinding
+
+class MedicationAdapter : ListAdapter<Medication, MedicationAdapter.MedicationViewHolder>(MedicationDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MedicationViewHolder {
+        val binding = ItemMedicationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MedicationViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MedicationViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class MedicationViewHolder(
+        private val binding: ItemMedicationBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(medication: Medication) {
+            binding.tvName.text = medication.name
+            binding.tvDosageValue.text= medication.dosage
+            binding.tvFrequencyValue.text = medication.frequency
+            binding.tvTimeOfDayValue.text = medication.timeOfDay
+            binding.tvStartDateValue.text = medication.startDate
+            binding.tvEndDateValue.text = medication.endDate
+            binding.tvNoteValue.text = medication.note
+        }
+    }
+}
+
+class MedicationDiffCallback : DiffUtil.ItemCallback<Medication>() {
+    override fun areItemsTheSame(oldItem: Medication, newItem: Medication): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: Medication, newItem: Medication): Boolean {
+        return oldItem == newItem
+    }
+}
---
+++ app/src/main/java/com/example/healthcareproject/present/setting/SettingItem.kt
@@ -0,0 +1,7 @@
+package com.example.healthcareproject.present.setting
+
+data class SettingItem(
+    val id: Int,
+    val title: String,
+    val icon: Int
+)
